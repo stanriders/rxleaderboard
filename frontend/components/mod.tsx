f448f2b54de60b946f470e9f97c27883f561e845
StanR <hi@stanr.info>
2024-10-17 15:29:54 +0500

    Account for rate changes

--- frontend/components/mod.tsx
+++ frontend/components/mod.tsx
@@ -6,7 +6,7 @@ export const Mod: FC<Props> = (props) => {
   if (props.mod == "RX") {
     return <span className="text-default-200">{props.mod}</span>;
   }
-  else if (props.mod == "HD" || props.mod == "HR" || props.mod == "DT" || props.mod == "NC") {
+  else if (props.mod == "HD" || props.mod == "HR" || props.mod?.startsWith("DT") || props.mod?.startsWith("NC")) {
     return <span className="text-default-500">{props.mod}</span>;
   }
 
