919c5c3c3dae8faf50e95be7dff3598b7d33fe4f
StanR <hi@stanr.info>
2024-10-26 15:56:53 +0500

    Improve score display

--- frontend/components/mod.tsx
+++ frontend/components/mod.tsx
@@ -4,7 +4,7 @@ type Props = { mod: string | null };
 
 export const Mod: FC<Props> = (props) => {
   if (props.mod == "RX") {
-    return <span className="text-default-200">{props.mod}</span>;
+    return <span className="text-default-200 px-px">{props.mod}</span>;
   }
   else if (props.mod == "EZ" || 
             props.mod == "FL" || 
@@ -14,10 +14,10 @@ export const Mod: FC<Props> = (props) => {
             props.mod?.startsWith("NC") || 
             props.mod?.startsWith("HT") || 
             props.mod?.startsWith("DC")) {
-    return <span className="text-default-500">{props.mod}</span>;
+    return <span className="text-default-500 px-px">{props.mod}</span>;
   }
 
   return (
-    <span className="text-default-300">{props.mod}</span>
+    <span className="text-default-300 px-px">{props.mod}</span>
    );
 };
--- frontend/components/score.tsx
+++ frontend/components/score.tsx
@@ -7,6 +7,7 @@ import { formatDistance } from "date-fns";
 import { Spacer } from "@nextui-org/spacer";
 import { Mod } from "./mod";
 import { BeatmapModel, ScoreModel } from "@/api/types";
+import { Tooltip } from "@nextui-org/tooltip";
 
 type Props = { score: ScoreModel, showPlayer: boolean };
 
@@ -27,7 +28,7 @@ export const Score: FC<Props> = (props) => {
       
       <CardBody className="p-2">
         <div className="flex gap-2 items-center justify-center">
-          <div className="hidden md:block flex-none text-default-400 text-center w-10 text-lg">{score.grade}</div>
+          <div className="hidden md:block flex-none text-center w-10"><Link isExternal className="text-default-400 text-lg" href={`https://osu.ppy.sh/scores/${score.id}`}>{score.grade}</Link></div>
           <div className="flex flex-col flex-auto min-w-1">
             <div><Link href={`/beatmaps/${score.beatmapId}`} className="text-primary-500 text-sm" size="sm">{beatmap.artist} - {beatmap.title}<Spacer x={2}/><span className="text-primary-200 text-xs pr-3">{beatmap.difficultyName}</span></Link></div>
             <div className="flex-auto truncate text-default-400 text-xs">{score.totalScore} · {score.combo}x ( {score.count300} / {score.count100} / {score.count50} / {score.countMiss} )</div>
@@ -36,14 +37,14 @@ export const Score: FC<Props> = (props) => {
           
           <div className="hidden md:block flex-none text-sm">{score.mods?.map(m => <Mod key={m} mod={m}/>)}</div>
           <div className="hidden md:block flex-none text-default-500 text-center text-sm w-16">{(score.accuracy * 100)?.toFixed(2) ?? (<>- </>)}%</div>
-          <div className="hidden md:block flex-none text-primary-300 items-center text-center w-24 text-lg font-semibold">{score.pp?.toFixed(0) ?? (<>- </>)}pp</div>
+          <Tooltip showArrow={true} content={score.pp?.toFixed(4)} delay={300}><div className="hidden md:block flex-none text-primary-300 items-center text-center w-24 text-lg font-semibold">{score.pp?.toFixed(0) ?? (<>- </>)}pp</div></Tooltip>
         </div>
       </CardBody>
       <CardFooter className="md:hidden flex gap-2 items-center justify-center">
-        <div className="flex-none text-default-400 text-center w-10 text-md">{score.grade}</div>
+        <div className="flex-none text-center w-10"><Link isExternal className="text-default-400 text-md" href={`https://osu.ppy.sh/scores/${score.id}`}>{score.grade}</Link></div>
         <div className="flex-auto text-default-500 text-right text-sm">{score.mods?.map(m => <Mod key={m} mod={m}/>)}</div>
         <div className="flex-none text-default-500 text-center text-sm">{(score.accuracy * 100)?.toFixed(2) ?? (<>- </>)}%</div>
-        <div className="flex-none text-primary-300 items-center text-center w-20 text-lg font-semibold">{score.pp?.toFixed(0) ?? (<>- </>)}pp</div>
+        <Tooltip showArrow={true} content={score.pp?.toFixed(4)} delay={300}><div className="flex-none text-primary-300 items-center text-center w-20 text-lg font-semibold">{score.pp?.toFixed(0) ?? (<>- </>)}pp</div></Tooltip>
       </CardFooter>
     </Card>
   );
