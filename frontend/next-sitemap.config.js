edf43d36fc1d154b37bbafe1217c48d69ef209cd
StanR <hi@stanr.info>
2024-10-28 02:45:50 +0500

    Fuck off chatgpt

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://rx.stanr.info',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: 'GPTBot',
+          disallow: '/',
+        },
+      ],
+    }
+}
