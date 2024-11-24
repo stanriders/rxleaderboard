d1dd3a0b679ab1da4ad430789aa684c6d7f42e2a
StanR <hi@stanr.info>
2024-11-25 00:06:46 +0500

    I need to set up a linter

--- frontend/app/beatmaps/listing.tsx
+++ frontend/app/beatmaps/listing.tsx
@@ -79,7 +79,7 @@ export default function BeatmapListing() {
       <Spacer y={4} />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {response.beatmaps?.map((row: ListingBeatmap, index: number) => (
-            <BeatmapCard beatmap={row}/>
+            <BeatmapCard beatmap={row} key={row.id}/>
         ))}
       </div>
       <Spacer y={4} />
