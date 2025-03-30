c525d380f780312e33913ee99afa37ed052024da
StanR <hi@stanr.info>
2025-03-30 21:45:15 +0500

    Let's test something

--- frontend/api/address.tsx
+++ frontend/api/address.tsx
@@ -1,3 +1,3 @@
-
+export const ApiBase: string = "http://backend:3001/api";
 //export const ApiBase: string = "http://localhost:5237/api";
-export const ApiBase: string = "https://rx.stanr.info/api";
+//export const ApiBase: string = "https://rx.stanr.info/api";
